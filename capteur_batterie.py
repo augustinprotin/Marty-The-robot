69d78eff21a4c3ae080a5cd6e270e0e4dde88ce0
augustinprotin <augugu78@gmail.com>
2025-06-04 14:58:02 +0200

    batterier et getmarty fonctionnels

--- capteur_batterie.py
+++ capteur_batterie.py
@@ -1,11 +1,11 @@
 from martypy import Marty
 from PyQt6.QtWidgets import QMessageBox
-
+from app import *
 
 
 def capteur_batterie(fenetre):
     try:
-        battery_remain = fenetre.my_marty.get_battery_remaining()
+        battery_remain = fenetre.getMartyFromWindow().get_battery_remaining()
     except Exception as e:
         QMessageBox.information(fenetre, "Batterie", f"Marty n est pas connecté\nErreur attrapée : {e}")
     else:

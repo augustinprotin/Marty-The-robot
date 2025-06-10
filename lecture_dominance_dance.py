6d4ad60d2caa1fe0a81d264819f7ffaca4418f61
elBaptiste <villeret.baptiste@gmail.com>
2025-06-10 09:07:44 +0200

    ajout fonction ecriture de fichier .dance en sequentiel

--- lecture_dominance_dance.py
+++ lecture_dominance_dance.py
@@ -2,10 +2,7 @@ from MartyClass import MartyTheRobot
 from app import *
 import time
 
-def lecture_dominance():
-    # Créer une instance de MartyTheRobot
-    #mon_marty = fenetre.getMartyFromWindow() + mettre fenetre en param d'entrées
-    mon_marty = MartyTheRobot("wifi", "192.168.0.101")
+def lecture_dominance(mon_marty):
 
     file = open("dominance.dance", "r")
     lines = file.readlines()
@@ -42,6 +39,36 @@ def lecture_dominance():
             for i in range(4*distance):
                 mon_marty.goingBackward()
 
+def ecriture_dominance(fichier):
+    fichier = open(fichier, "w")
+    fichier.write("SEQ 3\n")
+    while(True):
+        direction = input("choisissez une direction : ").strip().lower()
+        distance = (input("choisissez un nombre de pas compris entre 1 et 3 : ")).strip()
+        while(direction not in ["droite", "gauche", "avant", "arriere"] ):
+            print("veuillez taper une direction existante : ")
+            direction = input("choisissez une direction : ").strip().lower()
+        if(direction == "droite"):
+            fichier.write(distance+"R\n")
+        elif(direction == "gauche"):
+            fichier.write(distance+"L\n")
+        elif(direction == "avant"):
+            fichier.write(distance+"U\n")
+        elif(direction == "arriere"):
+            fichier.write(distance+"B\n")
+
+        continuer = input("\nVoulez-vous détecter une autre couleur ? (o/n) : ")
+        if continuer.lower() != 'o':
+            fichier.close()
+            print("Fin du programme.")
+            break
+
+
+
 
 if __name__ == '__main__':
-    lecture_dominance()
+    # Créer une instance de MartyTheRobot
+    #mon_marty = fenetre.getMartyFromWindow() + mettre fenetre en param d'entrées
+    #mon_marty = MartyTheRobot("wifi", "192.168.0.101")
+    #lecture_dominance(mon_marty)
+    ecriture_dominance("creation.dance")
